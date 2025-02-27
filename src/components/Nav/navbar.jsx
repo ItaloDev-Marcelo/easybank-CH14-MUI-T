0b2be60064d5685cf36c12d9da3a5ba1b7d7b135
Italo Marcelo <devfrontend2028study@gmail.com>
2025-02-27 12:06:03 -0300

    up Nav

--- src/components/Nav/navbar.jsx
+++ src/components/Nav/navbar.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import { Button, Paper, Stack} from '@mui/material';
+import { Button, Paper, Stack,AppBar, Toolbar, IconButton, Box} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import Logo from '../../assets/images/logo.svg'
@@ -14,15 +14,52 @@ export default function Nav() {
     const navItem = ['Home', 'About','Contact', 'Blog', 'Careers'];
 
    return  (
-     <Stack component='header'  sx={{display: 'flex', padding: {
+     <AppBar  color=' #f3f4f6' sx={{display: 'flex', padding: {
         xs :'1.5em 1em',
-        md : '1.5em 2em'
-     }, flexDirection: 'row',justifyContent: {
+        md : '1em 1em'
+     }, flexDirection: 'row', justifyContent: {
         xs : 'space-between',
-        md: 'space-evenly'
+        md: 'space-around'
      }, alignContent: 'center', alignItems: 'center' }}  >
-     <a href='#'><img src={Logo} alt='' /></a>
-     <Button aria-label='Menu' sx={{
+     <Toolbar>
+      <IconButton id='noHover'>
+      <img src={Logo} alt='' />
+      </IconButton>
+
+
+     <Stack id='l'  spacing={0}  sx={{
+        display: {
+             xs : 'none',
+            md: 'flex'
+        },
+        marginRight: {
+            xs : '0',
+            md: '2em'
+        },
+        margin: '0 2.5em',
+        padding: '0 2em',
+        width: '350px'
+     }} direction='row'  p={2}>
+       {navItem.map((Item)  => {
+          return (
+            <Button sx={{textTransform: 'Capitalize'}} width='75px' className='button-link'  variant='none'>{Item}</Button>
+          ) 
+       })} 
+     </Stack>
+     <Stack   sx={{
+        display: {
+            xs : 'none',
+            md: 'flex'
+        },
+        
+        padding: '0 1em',
+     }}>
+       <Button variant='contained' sx={{background: 'rgb(49, 211, 92)'}}  className='circle' >Request Invetation</Button>
+     </Stack>
+
+     </Toolbar>
+
+     <Button aria-label='Menu-buton' sx={{
         display: {
             xs : 'flex',
             md: 'none'
@@ -33,48 +70,25 @@ export default function Nav() {
                 <MenuIcon sx={{color: 'gray'}} /> 
             }
      </Button>
-     <Stack id='l'  sx={{
-        display: {
-             xs : 'none',
-            md: 'flex'
-        },
-        marginRight: {
-            xs : '0',
-            md: '2em'
-        },
-        width: '350px'
-     }} direction='row' spacing={2} p={2}>
-       {navItem.map((Item)  => {
-          return (
-            <Button width='75px' className='button-link'  variant='none'>{Item}</Button>
-          ) 
-       })} 
-     </Stack>
-     <Stack spacing={2}  sx={{
-        display: {
-            xs : 'none',
-            md: 'flex'
-        }
-     }}>
-       <Button variant='contained' sx={{background: 'rgb(49, 211, 92)'}}  className='circle' >Request Invetation</Button>
-       </Stack>
-     <Paper id='navbar' elevation={12} component='nav' sx={{display: 
+
+     <Paper aria-label='NavBar' id='navbar' elevation={12} component='nav' sx={{display: 
         {
            xs:  toggle ? 'flex' : 'none',
            md: 'none',
-        }, justifyContent: 'center' , alignItems: 'center', position : 'absolute', top: '7em', height: '300px'}}>
+        }, justifyContent: 'center' , alignItems: 'center', position : 'absolute', top: '10em', height: '300px'}}>
         <List id='list' >
             {
              navItem.map((navItem, index) => {
                 return  (
                     <ListItem key={index}>
-                    <ListItemText > {navItem} </ListItemText>
+                    <ListItemText  sx={{textTransform: 'Capitalize'}} > {navItem} </ListItemText>
                     </ListItem>
                 ) 
              })
             }
         </List>
      </Paper>
-     </Stack>
+
+     </AppBar>
    )
 }
