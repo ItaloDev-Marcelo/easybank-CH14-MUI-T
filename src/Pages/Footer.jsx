9f335a5fb3d8f765a4d91134f50c2353ba7945ce
Italo Marcelo <devfrontend2028study@gmail.com>
2025-03-01 15:20:55 -0300

    footer up

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,19 +1,92 @@
 import React from 'react'
-import {Stack, Box} from '@mui/material';
+import {Stack, Box, IconButton, Typography, Button} from '@mui/material';
 import Grid from '@mui/material/Grid2';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import youtubeIcon from '../assets/images/icon-youtube.svg'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import logo from  '../assets/images/logo.svg'
 
 export default function Footer() {
+
+  const footerList = [  'About Us', 'Contact', 'Blog'];
+  const footerListTwo = [' Careers', 'Support', 'Privacy Policy'];
+
   return (
-    <Stack component='footer'  sx={{background: 'pink', padding: '2em'}} >
-      <Grid container >
-         <Grid sx={{background: 'red'}} size={{ xs: 12, md: 4 }}>
-            <Box>G1</Box>
+    <Stack component='footer'  sx={{padding: {
+       xs: '2em',
+       lg: '2em 8.5em'
+    }, color: '#fff'}} >
+      <Grid container  >
+         <Grid p={2} size={{ xs: 12, md: 4 }}>
+            <Stack component='div' direction='column' >
+            <Stack mb={3} justifyContent={{xs: 'center', md: 'flex-start'}}>
+              <img src={logo} alt='' id='footer-logo' />
+            </Stack>
+            <Stack  direction='row' justifyContent={{xs: 'center', md: 'flex-start'}} >
+               <Box>
+                  <IconButton aria-label='' >   
+                    <FacebookIcon className='icon' />                      
+                  </IconButton>
+               </Box>
+               <Box>
+                  <IconButton aria-label=''>   
+                     <img src={youtubeIcon} className='icon'/>                    
+                  </IconButton>
+               </Box>
+               <Box>
+                  <IconButton aria-label=''>   
+                    <TwitterIcon className='icon' />                      
+                  </IconButton>
+               </Box>
+               <Box>
+                  <IconButton aria-label=''>   
+                    <PinterestIcon className='icon' />                      
+                  </IconButton>
+               </Box>
+               <Box>
+                  <IconButton aria-label=''>   
+                    <InstagramIcon  className='icon'/>                      
+                  </IconButton>
+               </Box>
+            </Stack>
+            </Stack>
          </Grid>
-         <Grid sx={{background: 'blue'}} size={{ xs: 12, md: 5 }}>
-         <Box>H1</Box>
+         <Grid  size={{ xs: 12, md: 5 }}  >
+         <Stack  direction={{xs: 'column', md: 'row'}}   >
+            <Stack id='md1' >
+               <List>
+                  {
+                    footerList.map((item) => {
+                      return  <ListItem >
+                      <ListItemText>{item}</ListItemText>
+                   </ListItem>
+                    } )
+                  }
+               </List>
+            </Stack>
+            <Stack id='md2'>
+               <List>
+                  {
+                    footerListTwo.map((item) => {
+                      return  <ListItem>
+                      <ListItemText>{item}</ListItemText>
+                   </ListItem>
+                    } )
+                  }
+               </List>
+            </Stack>
+         </Stack>
          </Grid>
-         <Grid sx={{background: 'green'}} size={{ xs: 12, md: 3 }}>
-         <Box>D1</Box>
+         <Grid size={{ xs: 12, md: 3 }}>
+         <Box sx={{padding: '1em'}}>
+         <Button variant="contained"  className="circle"  sx={{textTransform: 'capitalize', marginBottom: '1.3em'}} > Request Invite</Button>
+         <Typography component='h6'>© Easybank. All Rights Reserved</Typography>
+         </Box>
          </Grid>
       </Grid>
     </Stack>
